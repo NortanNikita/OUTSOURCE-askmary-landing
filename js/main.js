b4fcb2db4793bcbbc663e9c721f7739cde11b633
NortanNikita <norttt69@gmail.com>
2020-09-16 13:39:49 +0300

    clearing files

--- js/main.js
+++
@@ -1,8 +0,0 @@
-function toggleMenu () {
-    $('#site-navigation').toggleClass('open')
-}
-
-function aboutMoreText () {
-    $('.featured-article__description').toggleClass('more')
-    $('.btn-more').hide()
-}
