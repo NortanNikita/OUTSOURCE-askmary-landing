6ce8486887bd77cca67600cee3dbc908d08f93be
NortanNikita <norttt69@gmail.com>
2020-09-16 13:29:25 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function toggleMenu () {
+    $('#site-navigation').toggleClass('open')
+}
+
+function aboutMoreText () {
+    $('.featured-article__description').toggleClass('more')
+    $('.btn-more').hide()
+}
